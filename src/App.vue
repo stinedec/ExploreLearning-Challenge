04a81bd05c730ef45d5ed79b7238fb546fcdfb11
stinedec <c.j.decarteret@gmail.com>
2023-05-11 14:42:19 -0600

    8 files modified, 3 added, 1 deleted

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,34 @@
 <script setup>
+import { ref } from 'vue';
+import draggable from 'vuedraggable';
 import Icon from './components/Icon.vue';
 
-const icons = [
+const icons = ref([
   'Label 1',
   'Label 2',
   'Label 3',
   'Label 4',
   'Label 5',
   'Label 6',
-];
+]);
 </script>
 
 <template>
-  <div class="container">
-    <Icon v-for="icon in icons" :label="icon" />
-  </div>
+  <draggable class="container" v-model="icons" tag="div" :delay="500">
+    <template #item="{ element: icon }">
+      <Icon :label="icon" />
+    </template>
+  </draggable>
 </template>
 
 <style scoped>
 .container {
   display: grid;
   grid-template-columns: 1fr 1fr;
+  place-items: center center;
   @media (min-width: 500px) {
     grid-template-columns: 1fr 1fr 1fr 1fr;
-    padding: 30px 60px;
+    padding: 20px 45px;
   }
   @media (min-width: 1000px) {
     grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
